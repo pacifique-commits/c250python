08bb574d1e6baaa4efb9f2e2846f498bd2229d99
pacifique-commits <typaccy@gmail.com>
2026-01-22 19:11:17 +0100

    dict

--- hogwarts.py
+++ hogwarts.py
@@ -1,8 +1,26 @@
-def main():
-   
-   spell()
-def spell():
-   n=int(input("Whats n: "))
-   for i in range(n):
-      print("Abracadabra")
-main()
+students=[
+    {
+        "name" : "Hermione",
+        "house" : "Gryfindore",
+        "patronous" : "Otter"
+     },
+     {
+       "name" : "Harry",
+       "house" : "Gryfindore",
+       "patronous" : "Stag"
+     },
+     {
+      "name" : "Ron",
+      "house" : "Gryfindore",
+      "patronous" : "Jack Russel terrier"
+      },
+      {
+      "name" : "draco",
+      "house" : "slytherine",
+      "patronous" : None,
+      }
+
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronous"] , sep=", ")
